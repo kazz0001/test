73347d2b8501b68c7681e48e865c33aeffd7bc6e
kazz0001 <102017591+kazz0001@users.noreply.github.com>
2023-10-28 14:16:30 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  var music = document.getElementById('music');
+  var bar = document.createElement('div');
+  bar.style.width = '100%';
+  bar.style.height = '5
